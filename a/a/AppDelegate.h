028951a2f35bfdd9c34a66496d3b8655dc5101d6
李聪会 <651505992@qq.com>
2016-03-07 14:08:39 +0800

    添加工程

---
+++ a/a/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  a
+//
+//  Created by 李聪会 on 16/3/7.
+//  Copyright © 2016年 com.xincheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ a/a/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  a
+//
+//  Created by 李聪会 on 16/3/7.
+//  Copyright © 2016年 com.xincheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
